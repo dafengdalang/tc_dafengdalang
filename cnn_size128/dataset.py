90d6ce7ee1e7f5778b2a872b737989e836d5b962
sUtomorrow <287233266@qq.com>
2018-07-22 15:28:19 +0800

    finish data sample and data segment

--- cnn_size128/dataset.py
+++ cnn_size128/dataset.py
@@ -5,6 +5,7 @@ from glob import glob
 import xml.etree.ElementTree as ET
 import shutil
 import pickle
+import pandas as pd
 
 def get_object_rect(tree_root):
     '''
@@ -24,14 +25,24 @@ class DataSet(object):
             if not os.path.exists(self.data_dir):
                 os.mkdir(self.data_dir)
             self.prepare_data(ori_data_dir)
-    
+            with open(os.path.join(self.data_dir, 'data_info.pkl'), 'rb') as fp:
+                self.data_info = pickle.load(fp)
+        else:
+            with open(os.path.join(self.data_dir, 'data_info.pkl'), 'rb') as fp:
+                self.data_info = pickle.load(fp)
+        
+        self.sample_points_df = None
+
+    def get_data_generator(self):
+        pass
+
     def prepare_data(self, ori_data_path):
         '''
         将类型编号，正常类为class0, 其他类从 class1 ~ class47, 将数据存入文件夹名称为类名的不同文件中
         数据信息统一存入data_info.pkl
         data_info.pkl format example：
         {
-            'class{id}':{
+            '{class_name}':{
                 'l' : {id},
                 'img_infos':[
                     {
@@ -97,15 +108,97 @@ class DataSet(object):
                 os.mkdir(class_data_dir)
             for img_info in class_info_dict[key]['img_infos']:
                 print(img_info['img_path'])
-                # shutil.copy(img_info['img_path'], os.path.join(class_data_dir, img_info['img_name'] + '.jpg'))
+                shutil.copy(img_info['img_path'], os.path.join(class_data_dir, img_info['img_name'] + '.jpg'))
                 del img_info['img_path']
         
         with open(os.path.join(self.data_dir, 'data_info.pkl'), 'wb') as fp:
             pickle.dump(class_info_dict, fp)
 
+
+    def get_sample_points_per_img(self, img_size_xy, class_rects, sample_point_num = None):
+        '''
+        从一张图片中获取采样点
+        '''
+        if class_rects is None or len(class_rects) == 0:
+            # 正常类，没有位置
+            if sample_point_num is None:
+                sample_point_num = 50
+            x_sample_list = np.random.choice(list(range(img_size_xy[0])), size = sample_point_num)
+            y_sample_list = np.random.choice(list(range(img_size_xy[1])), size = sample_point_num)
+            
+            sample_list_xy = [(y_sample_list[i], x_sample_list[i]) for i in range(sample_point_num)]
+
+            return sample_list_xy
+        else:
+            # 暂时在非正常类的图片中只采样瑕疵点
+            rect_point_list = [(x, y) for rect in class_rects for y in range(rect[1], rect[3]) for x in range(rect[0], rect[2])]
+            area = 0
+            for rect in class_rects:
+                area += (rect[3] - rect[1]) * (rect[2] - rect[0])
+            rect_sample_point_num = min(500, max(int(area * 0.005), 50))
+            rect_sample_idx_list = np.random.choice(list(range(len(rect_point_list))), size = rect_sample_point_num, replace = False)
+            rect_sample_points = []
+            for idx in rect_sample_idx_list:
+                rect_sample_points.append(rect_point_list[idx])
+
+            return rect_sample_points
+
+            # x_sample_list = np.random.choice(range(img_size_xy[0]), size = sample_point_num)
+            # y_sample_list = np.random.choice(range(img_size_xy[1]), size = sample_point_num)
+            
+            # sample_list_yx = [(x_sample_list[i], y_sample_list[i]) for i in range(sample_point_num)]
+
+
+    def get_sample_point(self):
+        '''
+        获取采样点信息
+        '''
+        if self.sample_points_df is None:
+            sample_point_csv_path = os.path.join(self.data_dir, 'sample_point.pkl')
+            if not os.path.exists(sample_point_csv_path):
+                print('cannot found sample point record csv, try to generate one')
+                sample_point_csv = {'img_path': [], 'coordX': [], 'coordY': [], 'l': []}
+                for key in self.data_info:
+                    for img_info in self.data_info[key]['img_infos']:
+                        img_path = os.path.join('class%d' % self.data_info[key]['l'], img_info['img_name'] + '.jpg')
+                        print('img:%s' % img_path)
+                        sample_point_list = self.get_sample_points_per_img((1920, 2560), img_info['rects'])
+                        print('sample point num:%d' % len(sample_point_list))
+                        for sample_point in sample_point_list:
+                            sample_point_csv['img_path'].append(img_path)
+                            sample_point_csv['coordX'].append(sample_point[0])
+                            sample_point_csv['coordY'].append(sample_point[1])
+                            sample_point_csv['l'].append(self.data_info[key]['l'])
+                self.sample_points_df = pd.DataFrame(sample_point_csv)
+                self.sample_points_df = self.sample_points_df.drop_duplicates()
+                print('generate sample point info end, save as %s' % sample_point_csv_path)
+                self.sample_points_df.to_csv(sample_point_csv_path, index = False)
+            else:
+                print('use sample point record:%s' % sample_point_csv_path)
+                self.sample_points_df = pd.read_csv(sample_point_csv_path)
+        
+        return self.sample_points_df
+        
+
+    def split_data(self, eval_rate = 0.1, test_rate = 0.1):
+        sample_point_df = self.get_sample_point()
+        point_num = len(sample_point_df)
+        sample_point_df = sample_point_df.sample(n = point_num)
+        train_sample_point_df = sample_point_df.iloc[0 : int(point_num * ( 1 - eval_rate - test_rate))]
+        eval_sample_point_df = sample_point_df.iloc[int(point_num * ( 1 - eval_rate - test_rate)) : int(point_num * ( 1 - test_rate))]
+        test_sample_point_df = sample_point_df.iloc[int(point_num * ( 1 - test_rate)) : ]
+
+        train_sample_point_df.to_csv(os.path.join(self.data_dir, 'train_sample.csv'), index = False)
+        eval_sample_point_df.to_csv(os.path.join(self.data_dir, 'eval_sample.csv'), index = False)
+        test_sample_point_df.to_csv(os.path.join(self.data_dir, 'test_sample.csv'), index = False)
+
+
 if __name__ == '__main__':
     if False:
         data_set = DataSet("D:\\data\\tc", True, 'D:\\data\\天池')
+    if True:
+        data_set = DataSet("D:\\data\\tc", False)
+        data_set.split_data()
 
     # with open('D:\\data\\tc', 'rb') as fp:
     #     data_info = pickle.load(fp)
